2ffb0a64b11058e10e652fbd62d921bb820d71e6
Yanael Barbier <y.barbier@sprout.ai>
2023-10-20 14:43:15 +0200

    Init project, SSE with hono

---
+++ app/src/index.ts
@@ -0,0 +1,39 @@
+import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+
+
+const app = new Hono()
+
+app.use('*', cors(
+    {
+        origin: '*',
+        allowMethods: ['GET'],
+        allowHeaders: ['Content-Type'],
+    }
+))
+
+app.get('/', (c) => c.text('Hello Hono!'))
+
+app.use('/sse/*', async (c, next) => {
+    c.header('Content-Type', 'text/event-stream');
+    c.header('Cache-Control', 'no-cache');
+    c.header('Connection', 'keep-alive');
+    await next();
+})
+
+app.get('/sse', (c) => {
+
+    return c.stream(async (stream) => {
+        stream.write('id: 0\n');
+        stream.write('data: hello\n\n');
+        
+        stream.write('id: 1\n');
+        stream.write('data: world\n\n');
+
+        stream.write('event: close\n');
+        stream.write('data: close\n\n');
+    })
+
+})
+
+export default app
